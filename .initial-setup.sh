ce52d622b76a749b36bdbcddd0945586092fc8ea
hiziri <meirvg+dev@gmail.com>
2026-06-04 16:04:26 +0900

    update

--- .initial-setup.sh
+++ .initial-setup.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
 # install aqua
+echo "START: initial setup"
+
 DIR=$(mktemp -d)
 cd $DIR
 
+echo "START: aqua install"
 wget "https://github.com/aquaproj/aqua/releases/latest/download/aqua_linux_amd64.tar.gz"
 tar xvf "aqua_linux_amd64.tar.gz"
 mkdir -p ~/.local/bin 2&>1 > /dev/null
 cp aqua ~/.local/bin/
 export PATH="$HOME/.local/bin:$PATH"
+echo "DONE: aqua install"
 
 # make global config file
+echo "START: setup aqua initial config"
 mkdir -p ~/.config/aqua 2&>1 > /dev/null
 export AQUA_GLOBAL_CONFIG="$HOME/.config/aqua/aqua.yaml"
 cd ~/.config/aqua/
 aqua init
+echo "DONE: setup aqua initial config"
 
-# install bitwarden
+# install essential tools
+echo "START: install chezmoi with bitwarden, mise via aqua"
 aqua up -r
 aqua g -g bitwarden/clients
+aqua g -g jdx/mise
 aqua g -g twpayne/chezmoi
 aqua i -a
 
 export PATH="$(aqua root-dir)/bin:$PATH"
-echo "installed aqua and done chezmoi setup"
+echo "DONE: install chezmoi with bitwarden, mise via aqua"
+
+echo "START: chezmoi init"
+MY_REPO="4hiziri/my-chezmoi"
+chezmoi init $MY_REPO
+chezmoi apply # need password of bitwarden
