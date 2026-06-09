b937779fc75652137e94e5a053bb8475f3d89dec
hiziri <meirvg+dev@gmail.com>
2026-06-09 10:26:59 +0900

    update

--- .initial-setup-linux.sh
+++ .initial-setup-linux.sh
@@ -1,57 +1,77 @@
 #!/bin/bash
 
-# install aqua
-echo "START: initial setup"
+GREEN="\e[0;32m"
+RED="\e[0;31m"
+CLEAR="\033[0m"
 
+ebegin() {
+  last_emsg="$*"
+  echo -e "${GREEN}$1${CLEAR}" >&2
+}
+
+eend() {
+  if [ "$1" = 0 ] || [ $# -lt 1 ]; then
+    echo -e "${GREEN}$last_emsg: ok${CLEAR}" >&2
+  else
+    shift
+    echo -e "${RED}$last_emsg: failed. $*${CLEAR}" >&2
+    echo "Error, exit"
+    exit 1
+  fi
+}
+
+# install aqua
+ebegin "APT update"
 sudo apt update && sudo apt upgrade -y
+eend $?
 
 DIR=$(mktemp -d)
 cd $DIR
 
-echo "START: aqua install"
+ebegin "Install aqua"
 wget "https://github.com/aquaproj/aqua/releases/latest/download/aqua_linux_amd64.tar.gz"
 tar xf "aqua_linux_amd64.tar.gz"
 mkdir -p ~/.local/bin 2&>1 > /dev/null
 cp aqua ~/.local/bin/
 export PATH="$HOME/.local/bin:$PATH"
-echo "DONE: aqua install"
+eend $?
 
-# make global config file
-echo "START: setup aqua initial config"
+ebegin "Setup aqua initial config"
 mkdir -p ~/.config/aqua 2&>1 > /dev/null
 export AQUA_GLOBAL_CONFIG="$HOME/.config/aqua/aqua.yaml"
 cd ~/.config/aqua/
 aqua init
-echo "DONE: setup aqua initial config"
+eend $?
 
-# install essential tools
-echo "START: install chezmoi with bitwarden, mise via aqua"
+ebegin "Install chezmoi with bitwarden, mise via aqua"
 aqua up -r
 aqua g -g -i bitwarden/clients
 aqua g -g -i jdx/mise
 aqua g -g -i twpayne/chezmoi
 aqua i -a
+eend $?
 
 export PATH="$(aqua root-dir)/bin:$PATH"
-echo "DONE: install chezmoi with bitwarden, mise via aqua"
 
-echo "START: install tools"
-# linux part, for ruby, sbcl
+ebegin "Login bitwarden"
+export BW_SESSION=$(bw login --raw)
+eend $?
+
+ebegin "Install mise tools"
 sudo apt install -y libffi-dev libssl-dev libyaml-dev zlib1g-dev libzstd-dev
-
 mise plugin add sbcl https://github.com/mise-plugins/mise-sbcl
-
 mise --raw install # for sbcl build, need raw input/output
-echo "DONE: install tools"
+eend $?
 
-echo "START: chezmoi deply"
+ebegin "Deploy dotfiles"
 MY_REPO="4hiziri/my-chezmoi"
-bw login
 chezmoi init $MY_REPO
 chezmoi apply # need password of bitwarden
-echo "DONE: chezmoi deply"
+eend $?
 
-chsh /bin/zsh
+ebegin "Change shell"
+chsh $(whoami) -s /bin/zsh
+eend $?
 
 cd ~
 
