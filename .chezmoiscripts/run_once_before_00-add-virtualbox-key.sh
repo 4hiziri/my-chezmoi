6d2739755b65930de1e3ff32714fb4d3db461959
hiziri <meirvg+dev@gmail.com>
2026-06-10 15:04:45 +0900

    update

--- .chezmoiscripts/run_once_before_00-add-virtualbox-key.sh
+++ .chezmoiscripts/run_once_before_00-add-virtualbox-key.sh
@@ -8,5 +8,3 @@ fi
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian noble contrib' | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo chmod 644 /usr/share/keyrings/oracle-virtualbox-2016.gpg
-
-sudo apt update
