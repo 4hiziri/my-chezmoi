87a3f555ff3e97fa525252ab835ae62f6963baa4
hiziri <meirvg+dev@gmail.com>
2026-09-11 14:43:36 +0900

    update

--- dot_emacs.d/inits/90-doom-theme.el
+++ dot_emacs.d/inits/90-doom-theme.el
@@ -54,7 +54,6 @@
   (doom-modeline-env-rust-executable "rustc")
   :hook
   (elpaca-after-init . doom-modeline-mode)
-  (doom-modeline-mode . main)
   :config
   ;; parrot-mode保留、true/false、on/offなどのトグルができるモード
   ;; compilation保留、バックグラウンドでのテストやビルドの状態を表示
--- dot_emacs.d/inits/90-origami.el
+++
@@ -1,3 +0,0 @@
-(use-package s)
-(use-package origami
-  :after (dash s))
